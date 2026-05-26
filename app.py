f8fdce6920d24c9204707edecb2fc199fe4cef5e
alexsototailoring-dev <alexsototailoring@gmail.com>
2026-05-26 19:26:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,3 @@ with center:
         st.info("Choose a demo mode, then click Run Simulation.")
 
 
-
-                                                                              [ Read 66 lines ]
-^G Help          ^O Write Out     ^F Where Is      ^K Cut           ^T Execute       ^C Location      M-U Undo         M-A Set Mark     M-] To Bracket   M-B Previous
-^X Exit          ^R Read File     ^\ Replace       ^U Paste         ^J Justify       ^/ Go To Line    M-E Redo         M-6 Copy         ^B Where Was     M-F Next
